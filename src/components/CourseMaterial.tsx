29570509f60e04629d6f3f2ee3758a1a5317f8bf
Chanindu1117 <chanindukannangara1117@gmail.com>
2025-08-17 14:12:58 +0530

    project init

---
+++ src/components/CourseMaterial.tsx
@@ -0,0 +1,36 @@
+import Table from 'react-bootstrap/Table';
+export const CourseMaterial =() =>{
+    const tblHeaders : string[]=[
+        "Material ID",
+        "File Name",
+        "Material Name",
+        "Material ",
+        "Upload Time",
+        "Course ID",
+        "Option"
+    ];
+    return (
+        <Table striped bordered hover>
+      <thead>
+        <tr>
+          {tblHeaders.map((headings,index)=>(
+            <th key={index}>{headings}</th>
+          ))}
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>1</td>
+          <td>Mark</td>
+          <td>Otto</td>
+          <td>@mdo</td>
+          <td>gyg</td>
+          <td>hjj</td>
+          <td>@mdo</td>
+        </tr>
+       
+      </tbody>
+    </Table>
+
+    );
+}
